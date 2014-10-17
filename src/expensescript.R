330c513b9d7c8256bdbceda7aec859c620e58b61
Leg Days <legdays@gmail.com>
2014-10-17 18:49:39 -0400

    took out default r plotting, to be replaced with rcharts plotting

--- src/expensescript.R
+++ src/expensescript.R
@@ -12,19 +12,21 @@ source("src/plotData.r")
 
 # Import data!
 myItinerary <- getItinerary("data/destinations.csv")
+myCountryDayCountTable <- getCountryDayCountTable(myItinerary)
 myRawData <- getCashTrailsData("data/CashTrails-20141016_1720.csv")
 myFXRates <- getExchangeRates("data/fxrates.csv", unique(myRawData$Currency))
 
 myEnrichedCTData <- enrichCTData(myRawData, myItinerary, myFXRates)
 # Check border transactions
-myEnrichedCTData[is.na(myEnrichedCTData$Country),]
+# myEnrichedCTData[is.na(myEnrichedCTData$Country),]
 myFilteredCTData <- filterUnusualExpenses(myEnrichedCTData)
-
-myCountryDayCountTable <- getCountryDayCountTable(myItinerary)
 
 myTagCountryExpensePerDay <- constructTagCountryExpensePerDay(myFilteredCTData, myCountryDayCountTable)
 
-write.csv(myTagCountryExpensePerDay, file = "output/tagCountryExpensePerDay.csv")
+myTagCumSumPerTime <- constructTagCumSumPerTime(myFilteredCTData)
+ggplot(myTagCumSumPerTime, aes(x=UNIXTime,y=TagCumSum,group=Tag,fill=Tag)) + geom_area(position="stack")
+p2 <- nPlot(TagCumSum ~ UNIXTime, group = 'Tag', data = myTagCumSumPerTime, type = 'stackedAreaChart')
+p2
 
 plot(myEnrichedCTData$Date, cumsum(myEnrichedCTData$StandardizedAmount), type='h',
      col = as.factor(myEnrichedCTData$Country))
